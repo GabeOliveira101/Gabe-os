d6e2eb88221679637701dc70aa2fb993d20a46e2
GabeOliveira101 <oliveira.gabe1995@gmail.com>
2025-11-05 17:04:39 -0500

    Update service-worker.old.js

--- service-worker.old.js
+++ service-worker.old.js
@@ -1,32 +1,17 @@
-const CACHE_NAME="gabeos-v3-cache";
-const ASSETS=[
+const CACHE_NAME = "command-center-cache-v1";
+const ASSETS = [
+  "./",
   "index.html",
-  "app.js",
   "manifest.json"
 ];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch",e=>{
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request).then(fetchRes=>{
-        return caches.open(CACHE_NAME).then(cache=>{
-          cache.put(e.request,fetchRes.clone());
-          return fetchRes;
-        });
-      });
-    })
-  );
-});
-
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>{
-      return Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)));
-    })
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
 
